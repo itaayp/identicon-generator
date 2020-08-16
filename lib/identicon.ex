161eac4860ac0db4b67f65cc971e7ec870ba4a94
Itay Peceniski <itay.peck@gmail.com>
2020-08-16 10:30:10 -0300

    image struct created and its hex property is now used in hash_input

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,21 +4,23 @@ The module Identicon is responsible to handle all the functions that transforms 
 Identicon is the default profile picture of github. It is a symmetrical image containing .............
 """
 
-@doc """
-This function is responsible to call all the other functions in a pipe.
-It recieves a string as `input` and returns the `identicon`
-"""
+    @doc """
+    This function is responsible to call all the other functions in a pipe.
+    It recieves a string as `input` and returns the `identicon`
+    """
   def main(input) do
     input
     |> hash_input
 
   end
 
-@doc """
-This function transforms the `input` string in to a hash
-"""
+    @doc """
+    This function transforms the `input` string in to a hash
+    """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list  
+
+    %Identicon.Image{hex: hex}
   end
 end
---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule Identicon.Image do
+@moduledoc """
+The goal of this module is to hold the structs, that is, to hold the data inside the application
+"""
+    defstruct hex: nil
+end
