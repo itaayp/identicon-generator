1519a74b9cd2629d5074a819b48eb3f6acbfedaa
Itay Peceniski <itay.peck@gmail.com>
2020-08-16 19:15:50 -0300

    pixel_map property added to struct; positions of the even square values grabbed

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -14,6 +14,7 @@ Identicon is the default profile picture of github. It is a symmetrical image co
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
   end
 
     @doc """
@@ -62,7 +63,8 @@ Identicon is the default profile picture of github. It is a symmetrical image co
 
   @doc """
     This function filter just the even values of grid and replace it on the struct
-    It recieves the struct `image` as parameter and return the same struct with the property `grid` updated 
+    It recieves the struct `image` as parameter and return the same struct with the property `grid` updated.
+    This filter is needed because in our image we will paint just the even values 
   """
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
       grid = Enum.filter grid, fn({code, _index}) ->
@@ -70,6 +72,25 @@ Identicon is the default profile picture of github. It is a symmetrical image co
       end
 
       %Identicon.Image{image | grid: grid}
+  end
+
+  @doc """
+    This function maps the `top_left` and `bottom_right` pixels of each square that will be painted later.
+    The argument is the `image` struct.
+    The return of this function is `image` struct with the property `pixel_map` updated 
+  """
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+    end
+
+      %Identicon.Image{image | pixel_map: pixel_map}
   end
 
 end
--- lib/image.ex
+++ lib/image.ex
@@ -1,6 +1,10 @@
 defmodule Identicon.Image do
 @moduledoc """
-The goal of this module is to hold the structs, that is, to hold the data inside the application
+    This module is the `image` struct that will hold all the data inside the application
+    `hex property` is the array that contains all the data that generates the Identicon.
+    `color property` is the first 3 values of the `hex` and is the RGB color of the Identicon.
+    `grid property` is a array containing tuples with the value of the `hex` and it's position on it.
+    `pixel_map property` are the `top_left` and the `bottom_right` positions that the even values of `hex` will have on the `Identicon`.
 """
-    defstruct hex: nil, color: nil, grid: nil
+    defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
